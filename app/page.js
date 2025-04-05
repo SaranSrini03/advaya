dad1ad0171efb53de4171ee6ebec7616acd7f5a7
saransrini003 <saransrini003@gmail.com>
2025-04-05 12:15:56 +0530

    added design 2

--- app/page.js
+++ app/page.js
@@ -53,10 +53,13 @@ export default function HomePage() {
       <div className="relative z-10 flex items-center justify-center h-full text-white">
         <div className="text-center">
           <h2
-            className="text-sm sm:text-xl font-mono md:text-md font-semibold text-transparent bg-clip-text bg-gradient-to-r from-orange-600 to-yellow-500 mb-3 tracking-wide drop-shadow-md animate-fade-in"
+            className="text-bold sm:text-lg md:text-sm lg:text-sm xl:text-3xl font-mono font-semibold text-transparent bg-clip-text bg-gradient-to-r from-orange-600 to-yellow-500 mb-4 sm:mb-6 md:mb-8 leading-tight sm:leading-snug md:leading-normal text-center px-4 animate-fade-in drop-shadow-md"
           >
-            Department of Computer Science And Engineering  <span className="text-white">X</span> IEEE Student Branch Chapter - Computer Society
+            Department of Computer Science And Engineering
+            <span className="text-white"> X </span>
+            IEEE Student Branch Chapter - Computer Society
           </h2>
+
 
           <span
             className="glitch font-black font-mono cursor-pointer select-none mx-auto hover:scale-105 transition-transform duration-300"
